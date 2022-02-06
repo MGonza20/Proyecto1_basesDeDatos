b0490c71ce2abad320cb253520bc1f6a21c51cc8
Guillermo <guillermo@inlandlogistics.co>
2022-02-05 19:59:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto 01 - Bases de Datos
+
+## Programa para cargar los CSV en PostgreSQL
+
+Run program with `npm start`
---
+++ index.js
@@ -0,0 +1,76 @@
+// Libraries
+const fs = require('fs');
+const path = require('path');
+
+const csv = require('fast-csv');
+const colors = require('colors');
+const { Pool } = require('pg');
+const { createDraftCombineQuery } = require('./queries');
+// Constants
+const credentials = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'flights',
+    password: '2302',
+    port: 5432
+}
+
+// Function to read a csv file
+
+/**
+ * Reads a CSV file
+ * @param {string} fileName Name of file to read
+ * @param {string} tableName Name of the table to insert the data
+ */
+const crateTableFromFile = (fileName, tableName) => {
+    const parsedData = [];
+    fs.createReadStream(path.resolve(__dirname, 'files', `${fileName}.csv`))
+        .pipe(csv.parse({ headers: true }))
+        .on('error', error => console.log(error))
+        .on('data', async (row) => {
+            parsedData.push(row);
+        })
+        .on('end', async (count) => {
+            for (const row of parsedData) {
+                const pool = new Pool(credentials);
+                const values = Object.values(row).map(val => `'${val.replace("'", "''")}'`);
+                await pool.query(`
+                    INSERT INTO ${tableName} VALUES(
+                        ${values.join(",")}                    
+                    )
+                `);
+                pool.end();
+            }
+            console.log(`Inserted a total of ${count} rows into ${tableName}`.blue);
+        });
+
+}
+
+// Functions to create the tables
+
+const createTable = async (query) => {
+    try {
+        const pool = new Pool(credentials);
+        await pool.query(query);
+        pool.end();
+        console.log(`Created table`.blue);
+
+    } catch (error) {
+        console.log(`Error Creating: `.red, error);
+    }
+
+}
+
+
+
+
+
+// Insert Into Tables
+
+(async () => {
+    // Create Tables
+    await createTable(createDraftCombineQuery);
+    crateTableFromFile('Draft_Combine', 'draft_combine');
+})();
+
+
