050a8c901daa578917999edce789823f47ef2837
MGonza20 <64711979+MGonza20@users.noreply.github.com>
2022-02-24 12:50:10 -0600

    Respuesta 11 - Equipos con mas rebounds - Top 5

--- queriesF3.sql
+++ queriesF3.sql
@@ -3,6 +3,50 @@
 
 --- Queries - Fase 3
 
+
+---
+-- Respuesta 11 -- Equipos con mas rebounds -- Top 5
+---
+
+-- CAMBIO: <game>reb_home a FLOAT
+ALTER TABLE game
+ALTER COLUMN reb_home TYPE FLOAT USING reb_home::FLOAT
+-- CAMBIO: <game>reb_home a FLOAT
+
+-- CAMBIO: <game>reb_away a FLOAT
+ALTER TABLE game
+ALTER COLUMN reb_away TYPE FLOAT USING reb_away::FLOAT
+-- CAMBIO: <game>reb_away a FLOAT 
+
+UPDATE game
+SET reb_away ='0'
+WHERE reb_away = '';	
+
+UPDATE game
+SET reb_home ='0'
+WHERE reb_home = '';	
+
+-- Respuesta 11 -- Equipos con mas rebounds -- Top 5
+SELECT team_name, SUM(sub.rebounds) as team_rebounds
+FROM (
+	SELECT	team_name_home		AS	team_name,
+	SUM(reb_home)				AS	rebounds
+	FROM						game
+	WHERE						game_date >= '2015-01-01'
+	GROUP BY					team_name
+	UNION
+	SELECT	team_name_away		AS	team_name,
+	SUM(reb_away)				AS	rebounds
+	FROM						game
+	WHERE						game_date >= '2015-01-01'
+	GROUP BY					team_name
+	ORDER BY                    rebounds DESC
+) 	AS sub
+GROUP BY	team_name
+ORDER BY 	team_rebounds	DESC 
+LIMIT		5;
+
+
 --
 -- Respuesta 12 -- Equipos con mayores asistencias - Top 5  
 --
