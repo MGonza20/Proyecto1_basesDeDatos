a8ced381f0fc49915bf2065acfe456ba70ff3c41
= <guillermo.santosba@gmail.com>
2022-02-26 13:19:02 -0600

    Pushed readme

--- fase03/pregunta_01.sql
+++ fase03/pregunta_01.sql
@@ -4,7 +4,7 @@ SELECT player_attributes.team_id,
 	FROM team 
 	WHERE team.id = player_attributes.team_id
 ),
-percentile_cont(.50) WITHIN GROUP(ORDER BY (
+percentile_cont(.70) WITHIN GROUP(ORDER BY (
 	ROUND(
 	(player_attributes.weight/2.205)/
 	(((player_attributes.height)/39.37) * ((player_attributes.height)/39.37))
@@ -12,7 +12,7 @@ percentile_cont(.50) WITHIN GROUP(ORDER BY (
 )) as mediana_bmi
 FROM player
 LEFT JOIN player_attributes ON player_attributes.id = player.id 
-WHERE player.is_active = true
+WHERE player.is_active = true AND team_id > 0
 GROUP BY player_attributes.team_id
-ORDER BY mediana_bmi DESC
-LIMIT 5; 
+ORDER BY mediana_bmi asc
+LIMIT 10; 
